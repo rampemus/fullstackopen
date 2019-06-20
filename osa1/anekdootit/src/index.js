e1b0b68f5125fef4049cede865f8063c3e9171c1
rampemus <pasi.toivanen@live.com>
2019-06-20 14:19:07 +0300

    unicafe anekdootit divided into separate properly named folders

---
+++ osa1/anekdootit/src/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+const App = () => {
+    return (
+        <div>Anekdootit alkaa</div>
+    )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
