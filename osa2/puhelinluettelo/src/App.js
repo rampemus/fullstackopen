ad246bb0292a9b97d4e3f446dcb86c7e4b45541d
rampemus <pasi.toivanen@live.com>
2019-06-26 17:02:52 +0300

    Muistiinpanot and puhelinluettelo works through local json server part2 step6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/filter'
 import PersonForm from './components/personform'
 import Persons from './components/persons'
@@ -6,16 +7,19 @@ import Persons from './components/persons'
 const App = () => {
 
 
-    const [ persons, setPersons ] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
+    const [ persons, setPersons ] = useState([])
 
     const [ filterName, setFilterName ] = useState('')
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
+
+    useEffect(()=>{
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data)
+            })
+    },[])
 
     const nameTaken = (name) => {
         return persons.find(person=>person.name === name)
